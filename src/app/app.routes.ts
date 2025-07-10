b1982a669a66ed0c6416d1a82fc424414fbceb05
Emmanuel Manzanilla <pcoriente.dev.jrs@gmail.com>
2025-07-10 11:16:33 -0600

    importacion de appcomponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,5 @@
 import { Routes } from '@angular/router';
-import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: MainLayoutComponent,
-    children: [
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-      {
-        path: 'dashboard',
-        loadComponent: () =>
-          import('./pages/dashboard/dashboard.component').then(
-            (m) => m.DashboardComponent
-          ),
-      },
-      {
-        path: 'unidades',
-        loadComponent: () =>
-          import('./pages/transport-units/transport-units.component').then(
-            (m) => m.TransportUnitsComponent
-          ),
-      },
-    ],
-  },
-  { path: '**', redirectTo: '' }, // Redirige cualquier otra ruta a la página principal
+// Redirige cualquier otra ruta a la página principal
 ];
